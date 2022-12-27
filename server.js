8cb76d4dac8ada952bae782392b0573a5e885931
Alan Abraham <alabraha@tcd.ie>
2022-12-27 17:35:22 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const cors = require('cors');
+const hostname = '127.0.0.1';
+const port = 3000;
+const express = require('express')
+const app = express()
+
+var dataset;
+fs.readFile('./diamonds.csv', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  //console.log(data);
+  dataset = data;
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send(dataset)
+})
+
+app.use(cors({credentials: true, origin: true}));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
